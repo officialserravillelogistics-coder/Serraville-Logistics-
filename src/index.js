d5f10c6851b957ee06540b0fcc5a1fa4be272f37
officialserravillelogistics-coder <officialserravillelogistics@gmail.com>
2025-09-15 22:37:12 +0100

    Add entry point for React app

--- src/index.js
+++ src/index.js
@@ -6,7 +6,9 @@ import theme from './theme';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <ThemeProvider theme={theme}>
-    <App />
-  </ThemeProvider>
+  <React.StrictMode>
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>
 );
