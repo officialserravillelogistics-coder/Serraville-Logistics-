9e78e509fc40ba56f9676c95ccb7a252bf4a6c99
officialserravillelogistics-coder <officialserravillelogistics@gmail.com>
2025-09-15 22:34:00 +0100

    Add professional Sidebar navigation

---
+++ src/components/Sidebar.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import Drawer from '@mui/material/Drawer';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import DashboardIcon from '@mui/icons-material/Dashboard';
+import LocalShippingIcon from '@mui/icons-material/LocalShipping';
+
+const drawerWidth = 240;
+
+function Sidebar() {
+  return (
+    <Drawer
+      variant="permanent"
+      sx={{
+        width: drawerWidth,
+        flexShrink: 0,
+        '& .MuiDrawer-paper': {
+          width: drawerWidth,
+          boxSizing: 'border-box',
+        },
+      }}
+    >
+      <List>
+        <ListItem button>
+          <ListItemIcon>
+            <DashboardIcon />
+          </ListItemIcon>
+          <ListItemText primary="Dashboard" />
+        </ListItem>
+        <ListItem button>
+          <ListItemIcon>
+            <LocalShippingIcon />
+          </ListItemIcon>
+          <ListItemText primary="Shipments" />
+        </ListItem>
+      </List>
+    </Drawer>
+  );
+}
+
+export default Sidebar;
